c58671a093a82e19cd2cb22cee022d1d0c165434
s27471 <s27471@pjwstk.edu.pl>
2024-03-09 19:22:06 +0100

    max

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ Console.WriteLine("Hello, Yo2!");
 Console.WriteLine("Hello, Yo3!");
 
 Console.WriteLine(avg([1, 2, 3, 4, 5]));
+Console.WriteLine(getMax([1, 2, 3, 4, 5]));
 
 
 static double avg(int[] tab)
@@ -17,4 +18,18 @@ static double avg(int[] tab)
     }
 
     return sum / tab.Length;
+}
+
+static int getMax(int[] tab)
+{
+    int max = tab[0];
+    for(int i=1; i<tab.Length; i++)
+    {
+        if (tab[i] > max)
+        {
+            max = tab[i];
+        }
+    }
+
+    return max;
 }
