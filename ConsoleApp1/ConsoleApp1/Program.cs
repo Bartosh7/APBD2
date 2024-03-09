d6fbece002d900be26445b155f42301cf66573b3
s27471 <s27471@pjwstk.edu.pl>
2024-03-09 19:09:33 +0100

    average

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, Yo!");
+Console.WriteLine("Hello, Yo");
 Console.WriteLine("Hello, Yo1!");
 Console.WriteLine("Hello, Yo2!");
 Console.WriteLine("Hello, Yo3!");
+
+Console.WriteLine(avg([1, 2, 3, 4, 5]));
+
+
+static double avg(int[] tab)
+{
+    double sum = 0;
+    for (int i = 0; i < tab.Length; i++)
+    {
+        sum += tab[i];
+    }
+
+    return sum / tab.Length;
+}
